4e069d1d76b5f7325fa8c828df80fc052aedb904
Sourabh Jana <sourabh.jana@cubexo.io>
2023-03-23 01:58:58 +0530

    dfgh

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 
 const connectDB = () => {
-    mongoose.connect(`${process.env.DB_URI}`).then((data) => {
+    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
         console.log(`Mongodb is connected with server: ${data.connection.host}`)
     }).catch((error) => {
         console.log(error)
     })
 }
-
 module.exports = connectDB
